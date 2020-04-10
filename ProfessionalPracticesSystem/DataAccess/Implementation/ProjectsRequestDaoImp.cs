9573dcdbced3158ae952e4f75c6a28c610c6a107
SammyGCh <53235299+SammyGCh@users.noreply.github.com>
2020-04-10 12:49:42 -0500

    Update ProjectsRequestDaoImp.cs
    The reader was close.

--- ProfessionalPracticesSystem/DataAccess/Implementation/ProjectsRequestDaoImp.cs
+++ ProfessionalPracticesSystem/DataAccess/Implementation/ProjectsRequestDaoImp.cs
@@ -70,6 +70,7 @@ namespace DataAccess.Implementation
             }
             finally
             {
+                reader.Close();
                 connection.CloseConnection();
             }
 
