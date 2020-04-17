458e8a83a590ddb39074f719a68195b72af9ad27
SammyGCh <53235299+SammyGCh@users.noreply.github.com>
2020-04-16 20:46:17 -0500

    log4net configuration updated and DataAccessTests updated.

---
+++ ProfessionalPracticesSystem/DataAccessTests/OrganizationSectorDAOTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DataAccessTests
+{
+    [TestClass]
+    public class OrganizationSectorDAOTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
