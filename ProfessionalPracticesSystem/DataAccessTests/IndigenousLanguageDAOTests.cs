9b0e5c1d3b1e842f41013058005e3cbb9aa6f238
Pklove3459 <61339804+Pklove3459@users.noreply.github.com>
2020-04-24 18:13:18 -0500

    clasesTest

---
+++ ProfessionalPracticesSystem/DataAccessTests/IndigenousLanguageDAOTests.cs
@@ -0,0 +1,40 @@
+﻿/*
+    Date: 23/04/2020
+    Author(s) : César Sergio Martinez Palacios
+ */
+
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DataAccess.Implementation;
+using BusinessDomain;
+using System.Collections.Generic;
+using MySql.Data.MySqlClient;
+
+namespace DataAccessTests
+{
+    [TestClass]
+    public class IndigenousLanguageDAOTests
+    {
+
+        [TestMethod]
+        public void GetIndigenousLanguageById()
+        {
+            IndigenousLanguageDAO indigenousLanguageDao;
+            indigenousLanguageDao = new IndigenousLanguageDAO();
+            int idLanguage = 1;
+            IndigenousLanguage indigenousLanguage = indigenousLanguageDao.GetIndigenousLanguageById(idLanguage);
+
+            Assert.IsNull(indigenousLanguage);
+        }
+        [TestMethod]
+        public void GetAllIndigenousLanguages()
+        {
+            IndigenousLanguageDAO indigenousLanguageDao;
+            indigenousLanguageDao = new IndigenousLanguageDAO();
+            List<IndigenousLanguage> indigenousLanguages = indigenousLanguageDao.GetAllIndigenousLanguages();
+            int expectedResult = 4;
+            int obtainedResult = indigenousLanguages.Count;
+            Assert.AreEqual(expectedResult, obtainedResult);
+        }
+    }
+}
---
+++ ProfessionalPracticesSystem/DataAccessTests/MensualReporDAOTest.cs
@@ -0,0 +1,38 @@
+﻿/*
+    Date: 23/04/2020
+    Author(s) : César Sergio Martinez Palacios
+ */
+
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DataAccess.Implementation;
+using BusinessDomain;
+using System.Collections.Generic;
+using MySql.Data.MySqlClient;
+
+namespace DataAccessTests 
+{
+    [TestClass]
+    public class MensualReportDAOTest
+    {
+        [TestMethod]
+        public void GetMensualReportById()
+        {
+            MensualReportDAO mensualReportDao;
+            mensualReportDao = new MensualReportDAO();
+            int idMensualReport = 0;
+            MensualReport mensualReport = mensualReportDao.GetMensualReportById(idMensualReport);
+            Assert.IsNull(mensualReport);
+        }
+
+        public void GetAllMensualReport()
+        {
+            MensualReportDAO mensualReportDao;
+            mensualReportDao = new MensualReportDAO();
+            List<MensualReport> mensualReports = mensualReportDao.GetAllMensualReports();
+            int expectedResult = 1;
+            int obtainedResult = mensualReports.Count;
+            Assert.AreEqual(expectedResult, obtainedResult);
+        }
+    }
+}
