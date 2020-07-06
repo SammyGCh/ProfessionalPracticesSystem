f7081db1cabcaab898b981e4202564727dbf7b28
SammyGCh <53235299+SammyGCh@users.noreply.github.com>
2020-07-06 01:06:57 -0500

    New test classes creation

---
+++ ProfessionalPracticesSystem/DataAccessTests/InsertDataTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DataAccessTests
+{
+    [TestClass]
+    public class InsertDataTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
